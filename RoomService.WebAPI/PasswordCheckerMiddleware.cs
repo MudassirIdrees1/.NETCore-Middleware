91fb1b74f335c5daf63846c9e3c9b6f632ec758e
Mudassir Idrees <mudassir.idrees786@gmail.com>
2023-01-23 15:19:24 +0500

    Middleware With Unit Testing

---
+++ RoomService.WebAPI/PasswordCheckerMiddleware.cs
@@ -0,0 +1,73 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using System;
+using Microsoft.AspNetCore.Builder;
+using System.Net.Http;
+using System.Linq;
+
+namespace RoomService.WebAPI
+{
+    public class PasswordCheckerMiddleware
+    {
+        private const string APIKEYNAME = "passwordKey";
+
+        private readonly HttpClient Client;
+        private readonly RequestDelegate _next;
+        public PasswordCheckerMiddleware(RequestDelegate next, HttpClient Client)
+        {
+            this._next = next;
+            this.Client = Client;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            //Client.DefaultRequestHeaders.Add("passwordKey", "passwordKey123456789");
+            string Value = "passwordKey123456789";
+
+            try
+            {
+                if (!Client.DefaultRequestHeaders.TryGetValues(APIKEYNAME, out var passwordValue))
+                {
+                    context.Response.StatusCode = 403;
+                    context.Response.ContentType = "application/json";
+                    await context.Response.WriteAsync(JsonConvert.SerializeObject(new
+                    {
+                        statusCode = 403,
+                        isSuccess = false,
+                        message = "key is not available",
+                    }));
+                    return;
+                }
+                if (!Value.Equals(passwordValue.FirstOrDefault()))
+                {
+                    context.Response.StatusCode = 403;
+                    context.Response.ContentType = "application/json";
+                    await context.Response.WriteAsync(JsonConvert.SerializeObject(new
+                    {
+                        statusCode = 403,
+                        isSuccess = false,
+                        message = "Password Key!",
+                    }));
+                    return;
+                }
+
+                await this._next(context);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+
+    }
+    public static class PasswordCheckerMiddlewareExtensions
+    {
+        public static IApplicationBuilder UsePasswordMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<PasswordCheckerMiddleware>();
+        }
+    }
+}
---
+++ RoomService.WebAPI/Startup.cs
@@ -0,0 +1,61 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using RoomService.WebAPI.Data;
+using RoomService.WebAPI.Services;
+using System;
+using System.Net.Http;
+
+namespace RoomService.WebAPI
+{
+    public class Startup
+    {
+        public const string DatabaseFileName = "database.db";
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddScoped<IRoomsService, RoomsService>();
+            services.AddDbContext<RoomContext>(options => options.UseLazyLoadingProxies().UseSqlite($"Data Source={DatabaseFileName}"));
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+            services.AddControllers();
+            services.AddSingleton<HttpClient>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UsePasswordMiddleware();
+            //app.UseMiddleware<PasswordCheckerMiddleware>();
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
